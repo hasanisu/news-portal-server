11427e7898f7b53d45f986c10878562d89648351
hasanisu <hasanjimam@gmail.com>
2023-05-15 17:58:27 +0900

    added news data

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+
 const categories = require('./data/category.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News API Runnings');
@@ -12,6 +14,28 @@ app.get('/', (req, res) => {
 
 app.get('/news-catagories', (req, res) =>{
     res.send(categories)
+});
+
+app.get('/category/:id', (req, res)=>{
+    const id = req.params.id;
+
+    if(id === '08'){
+        res.send(news);
+    }
+    else{
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news)
+    }
+})
+
+app.get('/news', (req, res) =>{
+    res.send(news)
+})
+
+app.get('/news/:id',(req, res)=>{
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
 })
 
 app.listen(port, () => {
